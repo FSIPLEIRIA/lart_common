bb8e44be327291c533a8ca0c9d348523adcec9ce
carlostojal <carlos.tojal@hotmail.com>
2023-06-05 22:17:05 +0100

    fix cmake config

--- lart_commonConfig.cmake
+++ lart_commonConfig.cmake
@@ -1,12 +1,12 @@
 # Check if this file has already been included.
-if(NOT TARGET pcl_aggregator_core)
+if(NOT TARGET lart_common)
     # Define the target.
-    add_library(delaunay_path_planner_core SHARED IMPORTED)
-    set_target_properties(delaunay_path_planner_core PROPERTIES IMPORTED_LOCATION /usr/lib/delaunay_path_planner_core/libdelaunay_path_planner_core.so)
+    add_library(lart_common SHARED IMPORTED)
+    set_target_properties(lart_common PROPERTIES IMPORTED_LOCATION /usr/lib/lart_common/liblart_common.so)
 
     # Set the dependencies.
     find_package(CGAL REQUIRED)
 
-    target_include_directories(delaunay_path_planner_core INTERFACE /usr/include/delaunay_path_planner_core)
-    target_link_libraries(delaunay_path_planner_core INTERFACE ${CGAL_LIBRARIES})
+    target_include_directories(lart_common INTERFACE /usr/include/lart_common)
+    target_link_libraries(lart_common INTERFACE ${CGAL_LIBRARIES})
 endif()
